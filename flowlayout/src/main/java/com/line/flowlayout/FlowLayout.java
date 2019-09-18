bfd9df4780ec33cf8ccb5038a77f3988addcdd61
chenliu <1101587382@qq.com>
2019-09-18 16:07:18 +0800

    feature (view viewModel): add drag listener

--- flowlayout/src/main/java/com/line/flowlayout/FlowLayout.java
+++ flowlayout/src/main/java/com/line/flowlayout/FlowLayout.java
@@ -107,7 +107,9 @@ public class FlowLayout extends FrameLayout {
             @Override
             public void onViewReleased(@NonNull View releasedChild, float xvel, float yvel) {
                 super.onViewReleased(releasedChild, xvel, yvel);
-                dragListener.onReleased(releasedChild);
+                if (dragListener != null) {
+                    dragListener.onReleased(releasedChild);
+                }
                 int viewWidth = releasedChild.getWidth();
                 int viewHeight = releasedChild.getHeight();
                 int curLeft = releasedChild.getLeft();
